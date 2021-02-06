ec7e88beb310a90ed214c2e6da7857c50e0b8feb
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2021-02-05 21:12:55 -0500

    bug fixed

--- lib/Components/Custom_floatingButton.dart
+++ lib/Components/Custom_floatingButton.dart
@@ -4,21 +4,21 @@ import 'package:qr_scanner/models/scan_model.dart';
 import 'package:qr_scanner/providers/DB_provider.dart';
 
 class CustomFloatingButton extends StatelessWidget {
-  final DBProvider db =new DBProvider();
+  final DBProvider db = new DBProvider();
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      elevation: 0,
-      child: Icon(Icons.qr_code_scanner_outlined),
-      onPressed: () async {
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-            '#098673', 'Cancelar', false, ScanMode.QR);
-        print(barcodeScanRes);
-        ScanModel newScanModel = new ScanModel(value: barcodeScanRes);
-        db.addData(newScanModel);
-      }
+        elevation: 0,
+        child: Icon(Icons.qr_code_scanner_outlined),
+        onPressed: () async {
+          String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+              '#098673', 'Cancelar', false, ScanMode.QR);
+          print(barcodeScanRes);
+          ScanModel newScanModel = new ScanModel(value: barcodeScanRes);
+          db.addData(newScanModel);
+          db.updateStream(newScanModel);
+        }
     );
-    
   }
 }
